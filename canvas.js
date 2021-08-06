78700c35292575c7401c73e8b5645af85c0d132c
Aleksandr Sorokin <a.sorokin@daad-alumni.de>
2021-08-06 21:36:29 +0300

    Submitted app

--- canvas.js
+++ canvas.js
@@ -73,20 +73,36 @@ function Tracker(canvas) {
 
 	function onMove(event) {
 		event.preventDefault();
-		var coords = getCoords(event);
 		if (textBlock.isDragged) {
+			var coords = getCoords(event);
 			textBlock.x += coords.x - this.startX;
 			textBlock.y += coords.y - this.startY;
+			this.startX = coords.x;
+			this.startY = coords.y;
+			resetCanvas();
+			textBlock.render(ctx);
 		}
-		this.startX = coords.x;
-		this.startY = coords.y;
-		resetCanvas();
-		textBlock.render(ctx);
 	}
 
-	canvas.onmousedown = canvas.ontouchstart = onDown;
-	canvas.onmousemove = canvas.ontouchmove = onMove;
-	canvas.onmouseup = canvas.ontouchend = onUp;
+	this.start = function() {
+		canvas.addEventListener('mousedown', onDown);
+		canvas.addEventListener('mousemove', onMove);
+		canvas.addEventListener('mouseup', onUp);
+		canvas.addEventListener('touchstart', onDown);
+		canvas.addEventListener('touchmove', onMove);
+		canvas.addEventListener('touchstop', onUp);
+	}
+
+	this.stop = function() {
+		canvas.removeEventListener('mousedown', onDown);
+		canvas.removeEventListener('mousemove', onMove);
+		canvas.removeEventListener('mouseup', onUp);
+		canvas.removeEventListener('touchstart', onDown);
+		canvas.removeEventListener('touchmove', onMove);
+		canvas.removeEventListener('touchstop', onUp);
+	}
+
+	this.start();
 }
 
 /************************
@@ -97,9 +113,29 @@ function onCheck(event) {
 	var status = document.getElementById('status');
 	var dx = Math.abs(textBlock.x - 50);
 	var dy = Math.abs(textBlock.y - 100);
-	status.innerText = 'Текст и ссылки объединены в модуль. Расстояние от текста до ссылок больше, чем интерлиньяж текста';
+	status.innerText = 'По правилу якорных объектов абзац текста надо поместить в левый нижний угол страницы. Текст и ссылки должны быть достаточно близко друг к другу, чтобы образовать прямоугольный модуль.';
+	status.style.display = 'block';
 	
-	textBlock.moveTo(78, 463);
+	let correctImageURL = (
+		textBlock.x > 61 && textBlock.x < 69 &&
+		textBlock.y > 370 && textBlock.y < 386
+	) ? 'img/text-green.png' : 'img/text-red.png';
+	var correctBlock = new MovableItem(
+		correctImageURL, textBlock.x, textBlock.y);
+	console.log({x: textBlock.x, y: textBlock.y});
+	textBlock.moveTo(66, 384);
+	tracker.stop();
+	checkButton.style.display = 'none';
+	nextButton.style.display = 'inline';
+	nextButton.addEventListener('click', function() {
+		history.go(0);
+	});
+
+	// Google API: https://drive.google.com/uc?export=view&id=
+	// White text: 1CCQVkbqp02YQVZjhouKhbs7REn1YkhRN
+	// Green text: 1T4Jz8yuZ5aI31b72SpV3qUjLYapyW58F
+	// Red text:   15P7H8v_0SnebcBwxfbM_IUf51Ll992jY
+	// Backdrop:   1CMR_i0BgD5_G1pls9F3hynba1Vv9NE7c
 }
 
 /**********************
@@ -121,10 +157,11 @@ backdrop.onload = function() {
 	canvas.width = backdrop.width;
 	canvas.height = backdrop.height;
 	ctx.drawImage(backdrop, 0, 0);
-	textBlock = new MovableItem('img/text.png', 90, 350);
+	textBlock = new MovableItem('img/text.png', 250, 250);
 }
 
 var tracker = new Tracker(canvas);
 
 var checkButton = document.getElementById('check');
+var nextButton = document.getElementById('next');
 checkButton.onclick = onCheck;
