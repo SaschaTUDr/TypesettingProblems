e35a9b2eebeb18ee16872a75d535af1adebcace4
Aleksandr Sorokin <a.sorokin@daad-alumni.de>
2021-09-09 21:27:58 +0300

    Split blocks into parametrisable items

--- demo/js/spread.js
+++ demo/js/spread.js
@@ -1,59 +1,81 @@
-class SpreadableItem {
-	constructor(idHead, idSep, idText, trueSH, trueMH, trueST) {
-		this.head = document.getElementById(idHead);
-		this.text = document.getElementById(idText);
-		this.sep = document.getElementById(idSep);
-		this.current = null;
+class ParametrisedController {
+	constructor(array=[]) {
+		this.items = array;
+	}
+	add(item) {
+		this.items.push(item);
+	}
+	solve() {
+		this.items.forEach(function(item) {
+			item.solve();
+		});
+	}
+}
+
+class ParametrisedItem {
+	/*
+	Arguments
+	    id          Id of a parametrised element (e.g. 'text-2')
+	    property    CSS property to vary (e.g. 'line-height')
+	    trueValue   Expected value with units (e.g. '24px')
+	    minValue    Minimal allowed value without units (e.g. 6)
+	    dir         1 = vertical, 2 = horizontal, 3 = both
+	*/
+	constructor(id, property, trueValue, minValue=0, dir=1) {
+		this.item = document.getElementById(id);
+		this.property = property;
+		this.trueValue = trueValue;
+		this.minValue = minValue;
+		this.dir = dir;
 		this.isDragged = false;
+		this.x = 0;
 		this.y = 0;
-		this.trueSH = trueSH;
-		this.trueMH = trueMH;
-		this.trueST = trueST;
 		var that = this;
 
 		function mouseDown(event) {
 			event.preventDefault();
 			that.isDragged = true;
-			that.current = this;
+			that.x = event.clientX;
 			that.y = event.clientY;
 		}
+
 		function mouseUp(event) {
 			event.preventDefault();
 			that.isDragged = false;
-			that.current = null;
 		}
+
 		function mouseMove(event) {
 			event.preventDefault();
 			if (that.isDragged) {
+				let dx = event.clientX - that.x;
 				let dy = event.clientY - that.y;
-				if (that.current === that.head || that.current === that.text) {
-					let lh = parseFloat(that.current.style.lineHeight);
-					that.current.style.lineHeight = lh + dy/100.;
-				} else if (that.current === that.sep) {
-					let h = parseFloat(that.current.style.height);
-					if (h + dy >= 6)
-						that.current.style.height = h + dy + 'px';
+				let curValue = parseFloat(that.item.style[that.property]);
+				let suffix = '';
+				switch (that.property) {
+				case 'line-height':
+					dy /= 100.;
+					break;
+				case 'height':
+					suffix = 'px';
+					break;
+				default:
 				}
+				if ((that.dir & 1) == 1 && curValue + dy >= that.minValue)
+					that.item.style[that.property] = curValue + dy + suffix;
+				if ((that.dir & 2) == 2 && curValue + dx >= that.minValue)
+					that.item.style[that.property] = curValue + dx + suffix;
+				that.x = event.clientX;
 				that.y = event.clientY;
 			}
 		}
 
-		this.head.onmousedown = mouseDown;
-		this.sep.onmousedown = mouseDown;
-		this.text.onmousedown = mouseDown;
+		this.item.onmousedown = mouseDown;
 		window.addEventListener('mouseup', mouseUp);
 		window.addEventListener('mousemove', mouseMove);
 	}
 	solve() {
-		this.head.classList.add('spacing-transition');
-		this.sep.classList.add('height-transition');
-		this.text.classList.add('spacing-transition');
-		this.head.style.lineHeight = this.trueSH;
-		this.sep.style.height = this.trueMH + 'px';
-		this.text.style.lineHeight = this.trueST;
-
-		this.head.onmousedown = null;
-		this.sep.onmousedown = null;
-		this.text.onmousedown = null;
+		this.item.classList.add(this.property + '-transition');
+		this.item.style[this.property] = this.trueValue;
+		this.item.onmousedown = null;
 	}
 }
